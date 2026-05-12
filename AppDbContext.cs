c7ef218c3ca899a5db88a0a6d2c2d44198b3b4cf
URBANO2O <urbanoneto37@gmail.com>
2026-05-11 21:01:21 -0300

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,12 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetroAtivos324133695.Models;
 
 namespace PetroAtivos324133695.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ativo> Ativos { get; set; }
     }
 }
