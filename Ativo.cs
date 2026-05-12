71771bd79d705caf2bd4ba41042b66b97a89070b
URBANO2O <urbanoneto37@gmail.com>
2026-05-11 21:03:17 -0300

    Update Ativo.cs

--- Ativo.cs
+++ Ativo.cs
@@ -7,6 +7,12 @@ namespace PetroAtivos324133695.Models
 {
     public class Ativo
     {
-        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public string localizacao { get; set; }
+        public double CapacidadeProducao { get; set; }
+        public DateTime DataUltimaInspecao { get; set; }
+        public bool StatusOperacional { get; set; }
     }
 }
